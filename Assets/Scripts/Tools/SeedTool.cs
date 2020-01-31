afc12909e70d23cd26d3c2273a94dce3c032e327
Michiel Bos <michielbos97@hotmail.com>
2020-01-31 18:13:33 +0100

    Seeds can now also be planted on wet tiles.

--- Assets/Scripts/Tools/SeedTool.cs
+++ Assets/Scripts/Tools/SeedTool.cs
@@ -7,19 +7,20 @@ public class SeedTool : Tool {
     public CropData cropData;
     public PlantedCrop cropPrefab;
     public TileType soilType;
+    public TileType wetSoilType;
 
     public override bool CanUse(Player player, Vector2Int target) {
         Tilemap tilemap = TilemapManager.Instance.groundTilemap;
         Vector3Int position = new Vector3Int(target.x, target.y, 0);
         TileBase tile = tilemap.GetTile(position);
-        return soilType.Contains(tile);
+        return soilType.Contains(tile) || wetSoilType.Contains(tile);
     }
 
     public override void UseTool(Player player, Vector2Int target, GameObject toolObject) {
         Tilemap tilemap = TilemapManager.Instance.groundTilemap;
         Vector3Int position = new Vector3Int(target.x, target.y, 0);
         TileBase tile = tilemap.GetTile(position);
-        if (soilType.Contains(tile)) {
+        if (soilType.Contains(tile) || wetSoilType.Contains(tile)) {
             PlantedCrop crop = Instantiate(cropPrefab, target + new Vector2(0.5f, 0.5f), Quaternion.identity);
             crop.SetCrop(cropData);
         }
