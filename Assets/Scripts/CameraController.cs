9c7e98bac21cc6455381665e5a8a6ee622ad7ed9
Michiel Bos <michielbos97@hotmail.com>
2020-01-31 13:43:24 +0100

    The camera now follows the player.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 namespace HorseMoon {
 
 public class CameraController : MonoBehaviour {
+    public Transform target;
+
+    public float borderMargin = 0.3f;
+
     private new Camera camera;
+
     private void Start() {
         camera = GetComponent<Camera>();
         camera.transparencySortMode = TransparencySortMode.CustomAxis;
         camera.transparencySortAxis = new Vector3(0, 1, 0);
+    }
+
+    private void LateUpdate() {
+        FollowTarget();
+    }
+
+    private void FollowTarget() {
+        if (target == null)
+            return;
+        float cameraSize = camera.orthographicSize;
+        float maxYDist = cameraSize * (1f - borderMargin);
+        float maxXDist = maxYDist * camera.aspect;
+        Vector2 targetPos = target.position;
+        Vector2 cameraPos = camera.transform.position;
+        camera.transform.position = new Vector3(
+            Mathf.Clamp(cameraPos.x, targetPos.x - maxXDist, targetPos.x + maxXDist),
+            Mathf.Clamp(cameraPos.y, targetPos.y - maxYDist, targetPos.y + maxYDist),
+            -10
+        );
     }
 }
 
