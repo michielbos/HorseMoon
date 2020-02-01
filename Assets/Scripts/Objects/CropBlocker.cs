e0ba450638ee87d76013cfb758521b33f80e61d6
Michiel Bos <michielbos97@hotmail.com>
2020-02-01 01:27:05 +0100

    Applied sprites for sickle, weed and rocks.

--- Assets/Scripts/Objects/CropBlocker.cs
+++ Assets/Scripts/Objects/CropBlocker.cs
@@ -1,13 +1,19 @@
 using System;
 using HorseMoon;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Objects {
 
 public class CropBlocker : InteractionObject {
+    public Sprite[] sprites;
 
     private new void Start() {
         base.Start();
         CropManager.Instance.AddBlocker(transform.position.WorldToTile(), this);
+        if (sprites.Length > 0) {
+            renderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        }
     }
 
     private void OnDestroy() {
