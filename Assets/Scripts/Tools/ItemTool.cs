c381fb1d7edd990f8b775d55da35e3be3c391943
palm <tauman.exe@gmail.com>
2020-02-02 23:31:47 +0100

    ItemTool.cs formatting.

--- Assets/Scripts/Tools/ItemTool.cs
+++ Assets/Scripts/Tools/ItemTool.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 namespace HorseMoon.Tools {
 
-public class ItemTool : Tool {
-    public ItemInfo itemInfo;
+    public class ItemTool : Tool
+    {
+        public ItemInfo itemInfo;
 
-    public override bool CanUse(Player player, InteractionObject target) {
-        return target.objectType == ObjectType.ShippingBin && itemInfo is FoodInfo foodInfo;
-    }
+        public override bool CanUse(Player player, InteractionObject target)
+        {
+            return target.objectType == ObjectType.ShippingBin && itemInfo is FoodInfo foodInfo;
+        }
 
-    public override void UseTool(Player player, InteractionObject target, GameObject toolObject) {
-        if (target.objectType == ObjectType.ShippingBin && itemInfo is FoodInfo foodInfo) {
-            ShipItem(player, foodInfo);
+        public override void UseTool(Player player, InteractionObject target, GameObject toolObject)
+        {
+            if (target.objectType == ObjectType.ShippingBin && itemInfo is FoodInfo foodInfo)
+            {
+                ShipItem(player, foodInfo);
+            }
+        }
+
+        private void ShipItem(Player player, FoodInfo foodInfo)
+        {
+            Bag bag = player.playerController.bag;
+            bag.Remove(itemInfo, 1);
+            ScoreManager.Instance.moneyInBin += foodInfo.shipValue;
         }
     }
-
-    private void ShipItem(Player player, FoodInfo foodInfo) {
-        Bag bag = player.playerController.bag;
-        bag.Remove(itemInfo, 1);
-        ScoreManager.Instance.moneyInBin += foodInfo.shipValue;
-    }
-}
 
 }
