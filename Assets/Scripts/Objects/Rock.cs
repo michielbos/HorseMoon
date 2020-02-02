0ac2bfee114f4d790d751739e96929ffe85bcac6
dmDash <dashmario64@gmail.com>
2020-02-01 23:13:26 -0500

    #2 Rocks should take 4 hits by default.

---
+++ Assets/Scripts/Objects/Rock.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HorseMoon.Objects
+{
+	public class Rock : MonoBehaviour
+	{
+		public int health = 4;
+		public int staminaCost = 3;
+	}
+}
