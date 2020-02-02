c2dca0dd5d52f6c205782017f78bd1bfa7451eb7
dmDash <dashmario64@gmail.com>
2020-02-02 15:38:15 -0500

    #14 Added forest color multiplier

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -50,6 +50,7 @@ namespace HorseMoon {
         public Color sunriseColor;
         public Color dayColor;
         public Color sunsetColor;
+        public Color forestColorMultiplier;
 
         private void Start()
         {
@@ -85,37 +86,43 @@ namespace HorseMoon {
         private void UpdateSun()
         {
             // CONCEPT TEST, this should be replaced with a better method. -->
-            if (sunlight == null)
+            if (sunlight == null || Camera.main == null)
                 return;
+
+            Color multiplierColor = GetIsInForest(Camera.main.transform.position) ? forestColorMultiplier : Color.white;
 
             if (WorldTimeSeconds < HOUR * 5f) // 00:00 - 05:00 > Night
             {
-                sunlight.color = nightColor;
+                sunlight.color = nightColor * multiplierColor;
             }
             else if (WorldTimeSeconds < HOUR * 6f) // 05:00 - 06:00 > Night To Sunrise
             {
-                sunlight.color = Color.Lerp(nightColor, sunriseColor, (WorldTimeSeconds - HOUR * 5f) / HOUR);
+                sunlight.color = Color.Lerp(nightColor, sunriseColor, (WorldTimeSeconds - HOUR * 5f) / HOUR) * multiplierColor;
             }
             else if (WorldTimeSeconds < HOUR * 7f) // 06:00 - 07:00 > Sunrise to Day
             {
-                sunlight.color = Color.Lerp(sunriseColor, dayColor, (WorldTimeSeconds - HOUR * 6f) / HOUR);
+                sunlight.color = Color.Lerp(sunriseColor, dayColor, (WorldTimeSeconds - HOUR * 6f) / HOUR) * multiplierColor;
             }
             else if (WorldTimeSeconds < HOUR * 18f) // 07:00 - 18:00 > Day
             {
-                sunlight.color = dayColor;
+                sunlight.color = dayColor * multiplierColor;
             }
             else if (WorldTimeSeconds < HOUR * 19f) // 18:00 - 19:00 > Day To Sunset
             {
-                sunlight.color = Color.Lerp(dayColor, sunsetColor, (WorldTimeSeconds - HOUR * 18f) / HOUR);
+                sunlight.color = Color.Lerp(dayColor, sunsetColor, (WorldTimeSeconds - HOUR * 18f) / HOUR) * multiplierColor;
             }
             else if (WorldTimeSeconds < HOUR * 20f + MINUTE * 30f) // 19:00 - 20:30 > Sunset to Night
             {
-                sunlight.color = Color.Lerp(sunsetColor, nightColor, (WorldTimeSeconds - HOUR * 19f) / (HOUR + MINUTE * 30f));
+                sunlight.color = Color.Lerp(sunsetColor, nightColor, (WorldTimeSeconds - HOUR * 19f) / (HOUR + MINUTE * 30f)) * multiplierColor;
             }
             else // 20:00 - 24:00 > Night
             {
-                sunlight.color = nightColor;
+                sunlight.color = nightColor * multiplierColor;
             }
+        }
+
+        private bool GetIsInForest(Vector3 pos) {
+            return pos.x < -150f;
         }
     }
 
