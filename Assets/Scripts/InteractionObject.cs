d24c8c2b7f53d2420bd41a8d14a95fb91e96f6c7
palm <tauman.exe@gmail.com>
2020-02-02 23:20:53 +0100

    Fixed Code Formatting in InteractionObject.cs.

--- Assets/Scripts/InteractionObject.cs
+++ Assets/Scripts/InteractionObject.cs
@@ -1,49 +1,60 @@
 using UnityEngine;
 
-namespace HorseMoon {
+namespace HorseMoon
+{
 
-public class InteractionObject : MonoBehaviour {
-    public ObjectType objectType;
-    private Color markedColor = Color.cyan;
-    
-    protected new SpriteRenderer renderer;
-    private Color spriteColor;
-    private bool targeted;
+    public class InteractionObject : MonoBehaviour
+    {
+        public ObjectType objectType;
+        private Color markedColor = Color.cyan;
 
-    protected void Start() {
-        renderer = GetComponent<SpriteRenderer>();
-    }
+        protected new SpriteRenderer renderer;
+        private Color spriteColor;
+        private bool targeted;
 
-    public virtual bool CanUse(Player player) {
-        return false;
-    }
-
-    public virtual void UseObject(Player player) {
-        // Replace for additional use behaviour.
-        // This is only called if the tool claims it can't use this object.
-    }
-
-    public void MarkTargeted(bool targeted) {
-        if (this.targeted) {
-            if (!targeted) {
-                renderer.color = spriteColor;
-            }
-        } else if (targeted) {
-            spriteColor = renderer.color;
-            renderer.color = markedColor;
+        protected void Start()
+        {
+            renderer = GetComponent<SpriteRenderer>();
         }
-        this.targeted = targeted;
-    }
-}
 
-public enum ObjectType {
-    Misc,
-    ShippingBin,
-    Well,
-    Rock,
-    Stump,
-    Weed,
-    Pickupable
-}
+        public virtual bool CanUse(Player player)
+        {
+            return false;
+        }
+
+        public virtual void UseObject(Player player)
+        {
+            // Replace for additional use behaviour.
+            // This is only called if the tool claims it can't use this object.
+        }
+
+        public void MarkTargeted(bool targeted)
+        {
+            if (this.targeted)
+            {
+                if (!targeted)
+                {
+                    renderer.color = spriteColor;
+                }
+            }
+            else if (targeted)
+            {
+                spriteColor = renderer.color;
+                renderer.color = markedColor;
+            }
+            this.targeted = targeted;
+        }
+    }
+
+    public enum ObjectType
+    {
+        Misc,
+        ShippingBin,
+        Well,
+        Rock,
+        Stump,
+        Weed,
+        Pickupable
+    }
 
 }
