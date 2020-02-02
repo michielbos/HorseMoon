5d659ee7b690adf8fcb68837df5e1c6437020fd4
Michiel Bos <michielbos97@hotmail.com>
2020-02-02 12:03:01 +0100

    Added stone storage.

--- Assets/Scripts/Objects/StoneStorage.cs
+++ Assets/Scripts/Objects/StoneStorage.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 namespace HorseMoon.Objects {
 
-public class WoodStorage : InteractionObject {
+public class StoneStorage : InteractionObject {
     public override bool CanUse(Player player) {
         return true;
     }
 
     public override void UseObject(Player player) {
-        SpeechUI.Instance.Behavior.ShowPopup("Current lumber in storage: " + ScoreManager.Instance.wood);
+        SpeechUI.Instance.Behavior.ShowPopup("Current stone in storage: " + ScoreManager.Instance.stones);
     }
 }
 
