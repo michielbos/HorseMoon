198b9e003d59d53aed6ad9f0700c28e382f34dbc
dmDash <dashmario64@gmail.com>
2020-02-03 12:50:00 -0500

    #13 Screen fade out on bed interaction

--- Assets/Scripts/Objects/Bed.cs
+++ Assets/Scripts/Objects/Bed.cs
@@ -1,15 +1,39 @@
+using HorseMoon.Inventory.UI;
+
 namespace HorseMoon.Objects {
 
-public class Bed : InteractionObject {
-    public override bool CanUse(Player player) {
-        return true;
-    }
+    public class Bed : InteractionObject {
+        public override bool CanUse(Player player) {
+            return true;
+        }
 
-    public override void UseObject(Player player) {
-        TimeController.Instance.NextDay();
-        player.Stamina = player.maxStamina;
-        GameSaver.Instance.SaveGame();
+        public override void UseObject(Player player)
+        {
+            FadeOut.Instance.fadedOut += OnFadedOut;
+            FadeOut.Instance.Begin();
+            TimeController.Instance.runWorldTime = false;
+            BagWindow.Instance.enabled = false;
+            Player.Instance.playerController.enabled = false;
+        }
+
+        private void OnFadedOut()
+        {
+            FadeOut.Instance.fadedOut -= OnFadedOut;
+            FadeOut.Instance.fadedIn += OnFadedIn;
+
+            TimeController.Instance.NextDay();
+            Player.Instance.Stamina = Player.Instance.maxStamina;
+            GameSaver.Instance.SaveGame();
+        }
+
+        private void OnFadedIn()
+        {
+            FadeOut.Instance.fadedIn -= OnFadedIn;
+
+            TimeController.Instance.runWorldTime = true;
+            BagWindow.Instance.enabled = true;
+            Player.Instance.playerController.enabled = true;
+        }
     }
-}
 
 }
